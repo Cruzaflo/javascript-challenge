11220dd417384cb05e9b12d0f3324df20096a69b
Cruzaflo <64674430+Cruzaflo@users.noreply.github.com>
2020-08-29 16:50:41 -0700

    populateTable function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,3 +6,12 @@ console.log(data)
 var tableBody = d3.select("tbody")
 
 
+function populateTable(input) {
+    tableBody.html("")
+    input.forEach(function (entry) {
+        newRow = tableBody.append("tr")
+        Object.values(entry).forEach(function(value){
+            newRow.append("td").text(value)
+        })
+    })
+}
